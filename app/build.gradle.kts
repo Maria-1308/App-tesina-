3fdc69aefd374aeba091f00f9c9ac2a062a121ad
Maria-1308 <2230230@upv.edu.mx>
2026-03-18 02:06:00 -0600

    arreglado importacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     implementation("androidx.compose.material:material-icons-extended")
-    
+    implementation(libs.androidx.compose.foundation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
